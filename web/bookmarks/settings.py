20e7381885a01a9dc608388e5536657b8d91a8ba
Andrey Sakhno <andrey.sakhno@gmail.com>
2020-06-23 18:09:29 +0300

    Added project to volumes of container web

--- web/bookmarks/settings.py
+++ web/bookmarks/settings.py
@@ -42,7 +42,7 @@ POSTGRES_PASSWORD = env("POSTGRES_PASSWORD")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["mysite.com", "localhost", "127.0.0.1", "0.0.0.0"]
 
 # Application definition
 
@@ -128,7 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "account/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
