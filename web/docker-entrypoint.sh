e82a29da6606c8be2bcb1fa6ca4c0691f12111f9
Andrey Sakhno <andrey.sakhno@gmail.com>
2020-07-15 05:36:03 +0300

    Added db bookmarks_db for postgres

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -5,7 +5,7 @@ python manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py migrate
+python manage.py migrate --run-syncdb
 
 # Start server
 #python manage.py runserver 0.0.0.0:8000
